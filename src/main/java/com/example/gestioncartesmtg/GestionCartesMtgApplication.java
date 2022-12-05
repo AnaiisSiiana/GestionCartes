d683a0ce86638ff46d8de95cfeaaf007b6d46c5d
Anais Marcq <Anais.marcq@outlook.fr>
2022-12-05 17:07:56 +0100

    Premier commit

---
+++ src/main/java/com/example/gestioncartesmtg/GestionCartesMtgApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestioncartesmtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCartesMtgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCartesMtgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestioncartesmtg/GestionCartesMtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestioncartesmtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCartesMtgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
